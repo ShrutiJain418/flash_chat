8109806d0c2b142b7b159bc627e4f0c6bef823fb
Shruti Jain <shruti21153054@akgec.ac.in>
2023-06-05 19:52:02 +0530

    first commit

---
+++ android/app/src/main/java/co/appbrewery/flash_chat/MainActivity.java
@@ -0,0 +1,10 @@
+package co.shrutijain.flash_chat;
+
+import io.flutter.embedding.android.FlutterActivity;
+public class MainActivity extends FlutterActivity {
+  // @Override
+  // protected void onCreate(Bundle savedInstanceState) {
+  //   super.onCreate(savedInstanceState);
+  //   GeneratedPluginRegistrant.registerWith(this);
+  // }
+}
